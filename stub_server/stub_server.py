f5bba6c55507d27c704bca44d202c4e05a9585fe
Arseniy <senylar803@gmail.com>
2025-02-14 01:14:23 +0300

    фиксы и всякое такое

--- stub_server/stub_server.py
+++ stub_server/stub_server.py
@@ -8,7 +8,7 @@ def gen():
     return 70
 
 
-@app.route('/server/<int:server_id>')
+@app.route('/stub/server/<int:server_id>')
 def metrics(server_id):
     return jsonify({"cpu": random.randint(90, 100), "mem": f"{random.randint(0, 10)}%", "disk": f"{random.randint(0, 100)}%",
          "uptime": f"{random.randint(1, 30)}d {random.randint(0, 23)}h {random.randint(0, 59)}m",'cpu':95,'mem':'93%','disk':'85%'})
